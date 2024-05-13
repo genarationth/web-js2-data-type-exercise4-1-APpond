4df3c25fe7c17753c293d24c9724076aace897d0
ApPond <you@example.com>
2024-05-13 14:43:53 +0700

    finished

---
+++ script.js
@@ -0,0 +1,15 @@
+const Product = {
+    product_id: '0', 
+    product_name: '',
+    quantity_in_stock: '',
+    unit_price: '',
+    product_description: {
+        product_brand: '',
+        size_or_quantity: '',
+        taste_or_scent: '',
+        colour: '',
+        additional_description: ''
+    }
+};
+
+console.log(Product.product_id);
